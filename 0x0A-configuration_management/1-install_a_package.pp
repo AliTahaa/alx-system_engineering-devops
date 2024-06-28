1ce7de0feb222c38a1393f2377f13d784857fe05
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-06-28 21:27:59 +0300

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,13 +5,13 @@ package { 'python3.8':
   provider => 'pip3',
 }
 
-package { 'Flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
 package { 'Werkzeug':
   ensure => '2.1.1',
   provider => 'pip3',
-  require => Package['Flask'],
+  require => Package['flask'],
 }
