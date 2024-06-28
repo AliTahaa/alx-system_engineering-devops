e84f98cc155de7eb247dcb8e11cdccae5f97f949
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-06-28 23:47:55 +0300

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Install an especific version of flask (2.1.0)
-class { 'python::pip': }
+class { 'python::pip': }  # Include the python::pip class
 
 pip { 'flask':
-  ensure => '2.1.0',
+  ensure => '2.1.0',  # Specify desired version
+  provider => 'pip3',  # Explicitly use pip3 provider
 }
