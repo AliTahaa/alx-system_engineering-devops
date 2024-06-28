0bf7f266f944089f15292507d4d6c5339af01e4d
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-06-28 23:51:31 +0300

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,22 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
+yumrepo { 'epel':
+  baseurl => 'https://dl.fedoraproject.org/pub/epel/8/x86_64/',
+  enabled => yes,
+}
+
 package { 'python38':
-  ensure => '3.8.10',
+  ensure => present,
+}
+
+class { 'python::pip': }
+
+pip { 'flask':
+  ensure => '2.1.0',
   provider => 'pip3',
 }
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
-package { 'Werkzeug':
+
+pip { 'Werkzeug':
   ensure => '2.1.1',
   provider => 'pip3',
   require => Package['flask'],
