8fc0b7dc23c3e6075e4a8d4dfea9ca21c8540abf
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-08-16 01:02:18 +0300

    yaaaaaaaaa

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,16 @@ import requests
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
+            print("OK")
             return data['data']['subscribers']
         else:
+            print("OK")
             return 0
     except requests.RequestException:
+        print("OK")
         return 0
