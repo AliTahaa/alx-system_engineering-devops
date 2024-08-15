6047692df36cde2ac860a551a05ebaff534a8c31
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-08-16 00:56:22 +0300

    tryyyyyyyy

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-""" Module """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns number of subscribers"""
-    import requests
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    raw_sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                                .format(subreddit),
-                                headers={"User-Agent": "My-User-Agent"},
-                                allow_redirects=False)
-    if raw_sub_info.status_code >= 300:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    return raw_sub_info.json().get("data").get("subscribers")
